204005bb2d1226b7377b1344f5538b73fd79af94
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-10-10 15:51:36 -0700

    test data implemented using faker js

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,38 +1,42 @@
 import { test } from "@playwright/test";
 import PageManager from "../page-objects/pageManager";
+import { faker } from "@faker-js/faker";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 });
 
 test("Navigate to form page", async ({ page }) => {
-    const pm = new PageManager(page);
-    pm.navigateTo();
-    await pm.navigateTo().formLayoutsPage();
+  const pm = new PageManager(page);
+  pm.navigateTo();
+  await pm.navigateTo().formLayoutsPage();
 });
 
 test("Parameterized methods", async ({ page }) => {
+  const randomFullName = faker.person.fullName();
+  const ramdomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(
+    100
+  )}@test.com`;
 
   const pm = new PageManager(page);
   await pm.navigateTo().formLayoutsPage();
 
-  await pm.onFormsLayoutPage().submitUsingTheGridFormWithCredsAndSelectOption(
-    "test@test.com",
-    "Welcome1",
-    "Option 1"
-  );
-  await pm.onFormsLayoutPage().submitUsingInlineForm("Rash", "test@test.com", false);
+  await pm
+    .onFormsLayoutPage()
+    .submitUsingTheGridFormWithCredsAndSelectOption(
+      "test@test.com",
+      "Welcome1",
+      "Option 1"
+    );
 
+  await pm
+    .onFormsLayoutPage()
+    .submitUsingInlineForm(randomFullName, ramdomEmail, true);
 });
 
-
 test("Date picker - select number of dates from today", async ({ page }) => {
-
-    const pm = new PageManager(page);
-    await pm.navigateTo().datePickerPage();
-    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(7);
-    await pm.onDatepickerPage().selectDatePickerWithRange(7,14);
-
-  });
-
-
+  const pm = new PageManager(page);
+  await pm.navigateTo().datePickerPage();
+  await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(7);
+  await pm.onDatepickerPage().selectDatePickerWithRange(7, 14);
+});
